91651c968362c3c886f71371f86a7a2c2ae9cacd
Your Name <you@example.com>
2025-05-25 10:31:04 +0300

    first commit

---
+++ auth/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import  AbstractBaseUser, PermissionsMixin
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
+from src.settings import TIME_ZONE
+from .managers import CustomUserManager
+
+
+class CustomUser(AbstractBaseUser,PermissionsMixin):
+    email = models.EmailField(_("email address"),unique=True,blank=True)
+    username = models.CharField(max_length=200,unique=True)
+    phone_number = models.CharField(max_length=20,unique=True,blank=True)
+    full_name = models.CharField(max_length=200)
+
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["full_name"]
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
+
+
+
---
+++ auth/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+
+
+
+def sign_up_view(request):
+    pass
+
+def login_view(request):
+    pass
